e8cb0a7301606befa650e3c6005f95f596394509
leovt <leonhard.vogt@gmx.ch>
2023-09-30 21:05:42 +0200

    Bootstrap the web UI
    https://websockets.readthedocs.io/en/stable/intro/tutorial1.html

---
+++ http/main.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+
+import { createBoard, playMove } from "./connect4.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Initialize the UI.
+  const board = document.querySelector(".board");
+  createBoard(board);
+});
