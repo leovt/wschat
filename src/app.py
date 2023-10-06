10e49149824357c7119e83bc562a936eafb2b455
leovt <leonhard.vogt@gmx.ch>
2023-10-06 20:54:56 +0200

    add game logic

--- src/app.py
+++ src/app.py
@@ -2,35 +2,51 @@ import asyncio
 import websockets
 import json
 
-from connect4 import PLAYER1, PLAYER2
+from connect4 import PLAYER1, PLAYER2, Connect4
+
+class ValidationError(ValueError):
+    pass
+
+def validate(event):
+    if event['type'] != 'play':
+        raise ValidationError(f'Unexpected event type {event["type"]}')
 
 async def handler(websocket):
+    game = Connect4()
+    PLAYERS = [PLAYER1, PLAYER2]
+    turn = 0
+    moves_sent = 0
+
     async for message in websocket:
-        print(message)
+        try:
+            event = json.loads(message)
+            validate(event)
+            player = PLAYERS[turn % 2]
+            turn += 1
+            game.play(player, event['column'])
+            while len(game.moves) > moves_sent:
+                (player, column, row) = game.moves[moves_sent]
+                moves_sent += 1
 
-async def handler(websocket):
-    for player, column, row in [
-        (PLAYER1, 3, 0),
-        (PLAYER2, 3, 1),
-        (PLAYER1, 4, 0),
-        (PLAYER2, 4, 1),
-        (PLAYER1, 2, 0),
-        (PLAYER2, 1, 0),
-        (PLAYER1, 5, 0),
-    ]:
-        event = {
-            "type": "play",
-            "player": player,
-            "column": column,
-            "row": row,
-        }
-        await websocket.send(json.dumps(event))
-        await asyncio.sleep(0.5)
-    event = {
-        "type": "win",
-        "player": PLAYER1,
-    }
-    await websocket.send(json.dumps(event))
+                await websocket.send(json.dumps({
+                    'type': 'play',
+                    'player': player,
+                    'column': column,
+                    'row': row
+                }))
+
+            if game.winner:
+                await websocket.send(json.dumps({
+                    'type': 'win',
+                    'player': game.winner,
+                }))
+
+        except Exception as exception:
+            await websocket.send(json.dumps({
+                'type': 'error',
+                'message': repr(exception)
+            }))
+
 
 async def main():
     async with websockets.serve(handler, "", 8001):
